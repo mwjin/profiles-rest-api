a141ec099ee282d2f6b77eeec01ba15dff7dc915
Minwoo Jeong <jeongmwj@gmail.com>
2022-03-11 22:01:12 +0900

    Create `UserLoginApiView`

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import filters, status, viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
+from rest_framework.settings import api_settings
 from rest_framework.views import APIView
 
 from profiles_api import models, permissions, serializers
@@ -97,3 +99,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
         "name",
         "email",
     )
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
