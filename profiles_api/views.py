f231a9aff412b2f3d089213b9b76549e05b24c67
Minwoo Jeong <jeongmwj@gmail.com>
2022-03-12 17:09:36 +0900

    Add permissions to `ProfileFeedItemViewSet`

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import filters, status, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
 from rest_framework.views import APIView
@@ -111,6 +112,10 @@ class ProfileFeedItemViewSet(viewsets.ModelViewSet):
     """Handles creating, reading and updating profile feed items"""
 
     authentication_classes = (TokenAuthentication,)
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticatedOrReadOnly,
+    )
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
 
